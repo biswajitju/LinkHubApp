aa7493c6e72d36e9305ddd791f62505f04bfb298
Biswajit Das <biswajitdascdr@gmail.com>
2021-07-30 00:53:33 +0530

    created all controller respective areas and added default index views.

---
+++ LinkHubUI/Areas/Admin/Controllers/ListUsersController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LinkHubUI.Areas.Admin.Controllers
+{
+    public class ListUsersController : Controller
+    {
+        // GET: Admin/ListUsers
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
